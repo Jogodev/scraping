a3f0fd9bddd3ff5aa65bef20bfab512c3e4440c1
Jonathan Plastivène <jonathan.plastivene@outlook.com>
2022-09-17 22:30:37 +0200

    récuperation 1er livre et livre pour toute une categorie suite

--- scrap.py
+++ scrap.py
@@ -1,37 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+
 # CODE POUR UN LIVRE
-#url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-
-#url pour une catégorie
-url = "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
-
+url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 response = requests.get(url)
-
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# product_page_url = soup.find()
-# print(product_page_url)
-# title = soup.find('h1')
-# print(title)
-# image_url = soup.find('img')
-# print(image_url)
-# tabletd = soup.findAll('td')
-# print(tabletd)
+
+
+
+
+rating = soup.find('p', class_='star-rating Three').findAll('i')
+product_description = soup.find('meta', {'name': 'description'})
+category = soup.find('ul', class_='breadcrumb').find('a')
+title = soup.find('h1')
+image_url = soup.find('img')
+tablebook = soup.findAll('td')
+
+with open("books.csv", "w") as book:
+    book.write("product_page_url, category, title, product_description, universal_ product_code (upc), price_including_tax, "
+               "price_excluding_tax, number_available , review_rating, image_url \n")
+    book.write(str(category) + ' , ' + str(title) + ' , ' + str(product_description) + ' , ' + str(tablebook) + ' , ' + str(len(rating)) + ' , ' + str(image_url))
+
+# #url pour une catégorie
+# url = "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
 #
-# with open("books.csv", "w") as book:
-#     book.write("product_page_url, universal_ product_code (upc), title, price_including_tax, price_excluding_tax "
-#                ",number_available ,product_description, category, review_rating, image_url \n")
-#     book.write(str(title) + ' , ' + str(tabletd) + ' , ' + str(image_url
-
-
-category = soup.findAll('article', class_='product_pod')
-print(category)
-#[print(str(a) + '\n') for a in categs]
-#for li in categs:
-
-
-
-
+# category = soup.findAll('article', class_='product_pod')
+# print(category)
+# #[print(str(a) + '\n') for a in categs]
+# #for li in categs:
 
