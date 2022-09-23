4eda72c87600af43641e87e9979d61f6b4260321
Jonathan Plastivène <jonathan.plastivene@outlook.com>
2022-09-23 23:20:35 +0200

    récuperation 1er livre et livre pour toute une categorie suite

--- scrap.py
+++ scrap.py
@@ -1,28 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 import csv
 
 # CODE POUR UN LIVRE
-url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+
+product_page_url = []
+title = []
+product_description = []
+category = []
+review_rating = []
+image_url = []
+table_book = []
+
+url = "http://books.toscrape.com/catalogue/category/books/add-a-comment_18/index.html"
+
 response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.content, 'html.parser')
 
 
+def paginate():
+    page_number = soup.find('li', class_='next').find('a', href=True)
+    url_change = url.replace('index.html', page_number['href'])
+    next_page = requests.get(url_change)
+
+def scrap():
+
+    for book in urls:
+        #requests.get(urls)
+        product_page_url = soup.find('h3').find('a')
+        rating = soup.find('p', class_='star-rating Three').findAll('i')
+        product_description = soup.find('meta', {'name': 'description'}).text
+        category = soup.find('li', class_='active').text
+        title_book = soup.find('h3').text
+        table_book = soup.findAll('td')
+        print(urls)
+        with open("books.csv", "w") as books:
+            books.write(
+                "product_page_url ; category ; title ; product_description ; universal_ product_code (upc) ; price_including_tax ; "
+                "price_excluding_tax ; number_available  ; review_rating ; image_url \n")
+            books.write(str(product_page_url) + ' ; ' + str(category) + ' ; ' + str(title_book) + ' ; ' + str(
+                product_description) + ' ; ' + str(table_book) + ' ; ' + str(len(rating)))
 
 
-
-rating = soup.find('p', class_='star-rating Three').findAll('i')
-product_description = soup.find('meta', {'name': 'description'})
-category = soup.find('ul', class_='breadcrumb').find('a')
-title = soup.find('h1')
-image_url = soup.find('img')
-tablebook = soup.findAll('td')
-
-with open("books.csv", "w") as book:
-    book.write("product_page_url, category, title, product_description, universal_ product_code (upc), price_including_tax, "
-               "price_excluding_tax, number_available , review_rating, image_url \n")
-    book.write(str(category) + ' , ' + str(title) + ' , ' + str(product_description) + ' , ' + str(tablebook) + ' , ' + str(len(rating)) + ' , ' + str(image_url))
-
+scrap()
 # #url pour une catégorie
 # url = "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
 #
@@ -30,4 +51,7 @@ with open("books.csv", "w") as book:
 # print(category)
 # #[print(str(a) + '\n') for a in categs]
 # #for li in categs:
-
+# soup = BeautifulSoup(html)
+#
+# for a in soup.find_all('a', href=True):
+#     print("Found the URL:", a['href'])
