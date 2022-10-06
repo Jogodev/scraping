995f0ac126f5f66c489cb64fe1b4d608a8ee18e6
Jonathan Plastivène <jonathan.plastivene@outlook.com>
2022-10-07 00:11:13 +0200

    reprise scrap pour un livre

--- scrap.py
+++ scrap.py
@@ -15,7 +15,7 @@ table_book = []
 # url = "http://books.toscrape.com/catalogue/category/books/fantasy_19/index.html"
 # url = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
 url = "http://books.toscrape.com/index.html"
-response = requests.get()
+response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
 
@@ -33,31 +33,39 @@ def click(urls):
 #         print("Non")
 # paginate()
 
-def get_all_books(urls):
+def get_all_books():
     link_books = soup.find('ol', class_='row').findAll('a', href=True)
     for books in link_books:
         link_book = (('http://books.toscrape.com/') + (books['href']))
         book_pages = click(link_book)
         book_soup = BeautifulSoup(book_pages.content, 'html.parser')
-        rating = book_soup.find('p', class_='star-rating').findAll('i')
         categ = book_soup.find('li', class_='active').findPrevious('a').text
         title_book = book_soup.find('li', class_='active').text
         product_description = book_soup.find('div', {'id': 'product_description'}).findNext('p').text
-        table_book = book_soup.find('table', class_='table table-striped').text
+        table_book_upc = book_soup.find('table', class_='table table-striped').findNext('td').text
+        table_book_tax_excl = book_soup.find('table', class_='table table-striped').findNext('td').findNext('td').findNext('td').text
+        table_book_tax_incl = book_soup.find('table', class_='table table-striped').findNext('td').findNext('td').findNext('td').findNext('td').text
+        table_book_stock = book_soup.find('table', class_='table table-striped').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').text
+        rating = (book_soup.find('p', class_='star-rating').findAll('i'))
+        review = book_soup.find('table', class_='table table-striped').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').text
+
+
         with open("books.csv", "w") as bookfile:
             bookfile.write(
                 "product_page_url ; category ; title ; product_description ; universal_ product_code (upc) ; price_including_tax ; "
-                "price_excluding_tax ; number_available  ; review_rating ; image_url \n")
+                "price_excluding_tax ; number_available  ; rating ; review \n")
             bookfile.write(str(link_book) + ' ; ' + str(categ) + ' ; ' + str(title_book) + ' ; ' + str(
-                product_description) + ' ; ' + str(table_book) + ' ; ' + str(len(rating)))
+                product_description) + ' ; ' + str(table_book_upc) + ' ; ' + str(table_book_tax_excl) + ' ; ' + str(table_book_tax_incl) + ' ; ' + str(table_book_stock) + ' ; ' + str(len(rating)) + ' ; ' + str(review))
 
-get_all_books("http://books.toscrape.com/catalogue/category/books/christian-fiction_34/index.html")
-# def scrap():
+get_all_books()
+
+# def get_all_categories():
 #     categories = soup.find('div', class_='side_categories').find('ul').find('ul').findAll('a', href=True)
 #     for category in categories:
-#         category_soup = BeautifulSoup(response.content, 'html.parser')
 #         url_change_category = category['href'].replace("..", "")
 #         link_category = (('http://books.toscrape.com/') + (url_change_category))
 #         category_result = click(link_category)
-#         for category_result in category:
+#         category_soup = BeautifulSoup(category_result.content, 'html.parser')
+# get_all_categories()
+
 
