3a44ee874b00c595a32acfc03491eb3fdb719537
Jonathan Plastivène <jonathan.plastivene@outlook.com>
2022-10-07 02:12:02 +0200

    reprise scrap pour un livre avec csv plus propre

--- scrap.py
+++ scrap.py
@@ -39,23 +39,42 @@ def get_all_books():
         link_book = (('http://books.toscrape.com/') + (books['href']))
         book_pages = click(link_book)
         book_soup = BeautifulSoup(book_pages.content, 'html.parser')
-        categ = book_soup.find('li', class_='active').findPrevious('a').text
-        title_book = book_soup.find('li', class_='active').text
-        product_description = book_soup.find('div', {'id': 'product_description'}).findNext('p').text
-        table_book_upc = book_soup.find('table', class_='table table-striped').findNext('td').text
-        table_book_tax_excl = book_soup.find('table', class_='table table-striped').findNext('td').findNext('td').findNext('td').text
-        table_book_tax_incl = book_soup.find('table', class_='table table-striped').findNext('td').findNext('td').findNext('td').findNext('td').text
-        table_book_stock = book_soup.find('table', class_='table table-striped').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').text
+        categ = book_soup.find('li', class_='active').findPrevious('a')
+        title_book = book_soup.find('li', class_='active')
+        product_description = book_soup.find('div', {'id': 'product_description'}).findNext('p')
+        table_book_upc = book_soup.find('table', class_='table table-striped').findNext('td')
+        table_book_tax_excl = book_soup.find('table', class_='table table-striped').findNext('td').findNext('td').findNext('td')
+        table_book_tax_incl = book_soup.find('table', class_='table table-striped').findNext('td').findNext('td').findNext('td').findNext('td')
+        table_book_stock = book_soup.find('table', class_='table table-striped').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td')
         rating = (book_soup.find('p', class_='star-rating').findAll('i'))
-        review = book_soup.find('table', class_='table table-striped').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').text
-
-
+        review = book_soup.find('table', class_='table table-striped').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td')
+        header = ['product_page_url', 'category', 'title', 'product_description', 'upc', 'price_including_tax', 'price_excluding_tax', 'number_available', 'rating', 'review']
         with open("books.csv", "w") as bookfile:
-            bookfile.write(
-                "product_page_url ; category ; title ; product_description ; universal_ product_code (upc) ; price_including_tax ; "
-                "price_excluding_tax ; number_available  ; rating ; review \n")
-            bookfile.write(str(link_book) + ' ; ' + str(categ) + ' ; ' + str(title_book) + ' ; ' + str(
-                product_description) + ' ; ' + str(table_book_upc) + ' ; ' + str(table_book_tax_excl) + ' ; ' + str(table_book_tax_incl) + ' ; ' + str(table_book_stock) + ' ; ' + str(len(rating)) + ' ; ' + str(review))
+            writer = csv.writer(bookfile, delimiter=",")
+            writer.writerow(header)
+
+            for urls, categ, title_book, product_description, table_book_upc, table_book_tax_excl, table_book_tax_incl, table_book_stock, rating, review in zip(link_book, categ, title_book, product_description, table_book_upc, table_book_tax_excl, table_book_tax_incl, table_book_stock, str(len(rating)), review):
+                writer.writerow([link_book, categ, title_book, product_description, table_book_upc, table_book_tax_excl, table_book_tax_incl, table_book_stock, rating, review])
+
+
+
+
+
+
+
+
+
+        # with open("books.csv", "a") as bookfile:
+        #     bookfile.write(
+        #         "product_page_url ; category ; title ; product_description ; universal_ product_code (upc) ; price_including_tax ; "
+        #         "price_excluding_tax ; number_available  ; rating ; review \n")
+        #     bookfile.write(str(link_book) + ' ; ' + str(categ) + ' ; ' + str(title_book) + ' ; ' + str(
+        #         product_description) + ' ; ' + str(table_book_upc) + ' ; ' + str(table_book_tax_excl) + ' ; ' + str(
+        #         table_book_tax_incl) + ' ; ' + str(table_book_stock) + ' ; ' + str(len(rating)) + ' ; ' + str(review))
+
+
+
+
 
 get_all_books()
 
