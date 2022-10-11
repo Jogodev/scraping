402e6d1ab1c2db2a3750c8e825c0553cfdf0ec03
Jonathan Plastivène <jonathan.plastivene@outlook.com>
2022-10-11 14:48:55 +0200

    récuperation de toutes les images et plus de problèmes de lien(1 seul a la fois)

--- scrap.py
+++ scrap.py
@@ -1,7 +1,9 @@
 import requests
+import csv
+import urllib.request
+from PIL import Image
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-import csv
 
 product_page_url = []
 title = []
@@ -23,7 +25,7 @@ def paginate(url):
         try:
             print(footer.text.strip())
         except AttributeError as error:
-            print('aucune autre page')
+            print('')
         next_page = soup.select_one('li.next>a')
         if next_page:
             next_url = next_page.get('href')
@@ -35,8 +37,7 @@ def get_all_books(url):
     response = requests.get(url)
     book_soup = BeautifulSoup(response.content, 'html.parser')
     link_books = book_soup.find('ol', class_='row').findAll('a')
-    for books in link_books:
-        # link_book = (("http://books.toscrape.com/") + (books['href']))
+    for books in link_books[1::2]:
         book_url_change = books['href'].replace('../../../', '')
         link_book = (('http://books.toscrape.com/catalogue/') + (book_url_change))
         book_pages = click(link_book)
@@ -57,20 +58,22 @@ def get_all_books(url):
         rating = (book_soup.find('p', class_='star-rating').findAll('i'))
         review = book_soup.find('table', class_='table table-striped').findNext('td').findNext('td').findNext(
                 'td').findNext('td').findNext('td').findNext('td').findNext('td').text
+        image_url = book_soup.find('div', class_='item active').find('img')
+        image_url_replace = image_url['src'].replace("../../", "")
+        image_url_absolute = (("http://books.toscrape.com/") + (image_url_replace))
+        im = Image.open(urllib.request.urlopen(image_url_absolute))
+        im.save(f"{title_book}_image.png")
+
         with open(f"{categ}_books.csv", "a", encoding="utf-8") as csv_book:
-            header = ['product_page_url', 'category', 'title', 'product_description', 'upc', 'price_including_tax',
-                    'price_excluding_tax', 'number_available', 'rating', 'review']
             writer = csv.writer(csv_book)
-            writer.writerow(header)
             for product_page_url, category, title, product_description, upc, price_including_tax, price_excluding_tax, number_available, rating, review in zip(
-                    link_book, str(categ), str(title_book), product_description, str(table_book_upc),
+                    str(link_book), str(categ), str(title_book), product_description, str(table_book_upc),
                     str(table_book_tax_excl), str(table_book_tax_incl), str(table_book_stock), str(len(rating)),
                     review):
                 writer.writerow(
                     [link_book, categ, title_book, product_description, table_book_upc, table_book_tax_excl,
                     table_book_tax_incl, table_book_stock, rating, review])
     paginate(url)
-
 
 def get_all_categories(url):
     response = requests.get(url)
@@ -82,6 +85,13 @@ def get_all_categories(url):
         print(link_category)
         with open(f'{category.text.strip()}_books.csv', "w", encoding='utf-8') as csv_book:
             writer = csv.writer(csv_book)
+            header = ['product_page_url', 'category', 'title', 'product_description', 'upc', 'price_including_tax',
+                    'price_excluding_tax', 'number_available', 'rating', 'review']
+
             get_all_books(link_category)
 
 get_all_categories(url)
+
+
+
+# array[2]
